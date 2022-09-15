9fc83ed0ddcbb893039fe92d936f672a4241b8e5
violet666666 <setanhantu11111@gmail.com>
2022-09-15 19:28:13 +0700

    first commit

---
+++ ECMAScript 6/Deklarasi variabel/array and object.js
@@ -0,0 +1,17 @@
+const fruits = ['apple', 'orange'];
+fruits.push('banana');
+
+console.log(fruits);
+
+/* Output:
+[ "apple", "orange", "banana" ] 
+*/
+
+const people = { name: 'John', age: 18 };
+people.regency = 'Bandung';
+
+console.log(people);
+
+/* Output:
+{ name: "John", age: 18, regency: "Bandung" }
+*/
