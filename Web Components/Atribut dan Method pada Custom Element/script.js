be05af33923772fd08048587984e1e40814156d9
violet666666 <setanhantu11111@gmail.com>
2022-09-20 22:08:24 +0700

    add image figure custom tag

---
+++ Web Components/Atribut dan Method pada Custom Element/script.js
@@ -0,0 +1,7 @@
+const imageFigureElement = document.createElement('image-figure');
+
+imageFigureElement.setAttribute('src', 'https://i.imgur.com/iJq78XH.jpg');
+imageFigureElement.setAttribute('alt', 'Dicoding Logo');
+imageFigureElement.setAttribute('caption', 'Huruf g dalam logo Dicoding');
+
+document.body.appendChild(imageFigureElement);
---
+++ Web Components/Menulis Custom Element Pertama/image-figure.js
@@ -0,0 +1,3 @@
+class ImageFigure extends HTMLElement {}
+
+customElements.define('image-figure', ImageFigure);
